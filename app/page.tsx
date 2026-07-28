dd9a1cac6aa40d2b510662f72bbabcaf144d5ea7
adipal01111-web <adipal01111@gmail.com>
2026-07-28 15:10:58 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero";
+import MenuSection from "@/components/MenuSection";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <main className="relative">
+      <Hero />
+      <MenuSection />
+      <Footer />
+    </main>
+  );
+}
